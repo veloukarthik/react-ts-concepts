8005d184e019af50ec4f7c1f901bd5c7a75335a7
veloukarthik <71749429+veloukarthik@users.noreply.github.com>
2024-12-04 11:49:43 +0530

    user educer small update

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -4,7 +4,7 @@ const initialState = { count: 0 };
 
 type ACTIONTYPE =
   | { type: "increment"; payload: number }
-  | { type: "decrement"; payload: string };
+  | { type: "decrement"; payload: number };
 
 function reducer(state: typeof initialState, action: ACTIONTYPE) {
   switch (action.type) {
@@ -22,7 +22,7 @@ function Counter() {
   return (
     <div className="container mx-auto bg-indigo-900 text-center py-4 lg:px-4 text-white">
       <h1>Counter App</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => dispatch({ type: "decrement", payload: "5" })}>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => dispatch({ type: "decrement", payload: 5 })}>
         -
       </button>
        {state.count}
